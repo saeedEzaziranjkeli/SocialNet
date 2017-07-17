5ebe000f2000ade589bfe275d6f63cf5d9adb710
saeed EzaziRanjKeli <saeedezaziranjkeli@saeeds-mac.local>
2017-07-17 11:02:58 -0700

    Initial Commit

---
+++ SocialNet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialNet
+//
+//  Created by saeed EzaziRanjKeli on 17/07/17.
+//  Copyright © 2017 saeed EzaziRanjKeli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
